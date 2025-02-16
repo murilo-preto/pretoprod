39fba14a66fc3dd184cc52bc47f2f28c4ffdbbd6
Murilo Preto <murilopreto2003@Outlook.com>
2025-02-16 13:20:14 -0300

    fix: update log file path and enhance duration calculation logic

--- src/functions/mycalendar/utils.py
+++ src/functions/mycalendar/utils.py
@@ -1,9 +1,14 @@
 def calculate_duration_minutes(current_time, next_time) -> int:
-    """Calculate duration in minutes between two times"""
-    if next_time is None:  # Last event of the day
-        # Assume it lasts until the end of the workday if it's the last event
-        end_of_day = current_time.replace(hour=18, minute=0)  # 6 PM
-        return ((end_of_day.hour * 60 + end_of_day.minute) - 
-                (current_time.hour * 60 + current_time.minute))
-    return ((next_time.hour * 60 + next_time.minute) - 
-            (current_time.hour * 60 + current_time.minute))
+    """Calculate the duration in minutes between two times."""
+    
+    # Check if the next event time is provided
+    if next_time is None:
+        # If there is no next event, return a default duration of 10 minutes
+        return 10
+    
+    # If there is a next event, calculate the duration until that event
+    next_time_minutes = next_time.hour * 60 + next_time.minute
+    current_time_minutes = current_time.hour * 60 + current_time.minute
+    duration_minutes = next_time_minutes - current_time_minutes
+    
+    return duration_minutes
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from functions.parser import parse_work_logs
 from functions.mycalendar import save_week_calendar_view
 
 # Get all logs
-logs = parse_work_logs("makeshift_data")
+logs = parse_work_logs("src/makeshift_data")
 
 # Create the week view
 save_week_calendar_view(logs)
