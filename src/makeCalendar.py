99f58a980a6dc033ff76390ce6e7ef0900490247
Murilo Preto <murilopreto2003@Outlook.com>
2025-02-16 14:59:31 -0300

    feat: implement calendar functionality with HTML generation and styling updates

--- src/makeCalendar.py
+++ src/makeCalendar.py
@@ -1,8 +1,10 @@
 from functions.parser import parse_work_logs
 from functions.mycalendar import save_week_calendar_view
 
+data = "makeshift_data"
+
 # Get all logs
-logs = parse_work_logs("src/makeshift_data")
+logs = parse_work_logs(data)
 
 # Create the week view
 save_week_calendar_view(logs)
